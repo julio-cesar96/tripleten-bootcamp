d6d3b6d21c4e01edf40a9f584827cfa1d1b35f21
julio-cesar96 <julio_cbs96@hotmail.com>
2026-03-12 19:32:33 -0300

    feat: adicionar a header da pagina inicial

---
+++ cohort-31/sprint-7/semana-2/main.js
@@ -0,0 +1,25 @@
+// variavel - armazena um valor, que pode ser alterado posteriormente (let) ou não (const)
+const cpf = "123.456.789-00"; // string - texto, entre aspas
+const salario = 10000.00; // number - números, com ou sem casas decimais
+const estaAtivo = true; // boolean - true ou false
+
+const button = document.getElementById("button");
+button.addEventListener("click", function() {
+    const inputValue = inputBusca.value;
+    if (inputBusca === "") {
+    console.log("Por favor, digite o nome de uma série");
+    }
+});
+
+const inputBusca = document.getElementById("input");
+console.log(inputBusca.value);
+
+
+// estruturas condicionais - if, else if, else -> se, se não se, senão
+
+
+
+console.log(cpf);
+
+
+console.log("Hello, World!");
